f0c68921279e7249c7b2e2f9453c12d0db1ad85d
Mattias Törnqvist <mattias.trnqvist@gmail.com>
2020-06-11 20:50:45 +0200

    restructure file locations

---
+++ ItemFinder/Controller/Search Item/SearchTableDataSourceAndDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  SearchTableDataSourceAndDelegate.swift
+//  ItemFinderTests
+//
+//  Created by Mattias Törnqvist on 2020-06-11.
+//  Copyright © 2020 Mattias Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableDataSourceAndDelegate: NSObject, UITableViewDataSource, UITableViewDelegate {
+    
+    var delegate: SearchTableDelegate
+    var model = [DownloadedItem]()
+    
+    init(delegate: SearchTableDelegate) {
+        self.delegate = delegate
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: SearchItemTableViewCell.reuseIdentifier) as! SearchItemTableViewCell
+        
+        cell.item = model[indexPath.row]
+        
+        return cell
+    }
+    
+    func provideItemDataAndReloadTableView(_ tableView: UITableView, _ item: DownloadedItem) {
+        
+        let itemAlreadyExistsInModel = model.contains { (existingItem) -> Bool in
+            existingItem.itemId == item.itemId
+        }
+        
+        if itemAlreadyExistsInModel == false {
+            model.append(item)
+            print("DEBUG reloading")
+            tableView.reloadData()
+        }
+        
+    }
+    
+    
+}
---
+++ ItemFinder/Utils/SearchModule.swift
@@ -0,0 +1,96 @@
+//
+//  SearchModule.swift
+//  ItemFinderTests
+//
+//  Created by Mattias Törnqvist on 2020-06-11.
+//  Copyright © 2020 Mattias Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class SearchModule: NSObject {
+    
+    private var currentCountry: String?
+    private var currentAdminArea: String?
+    private var currentCity: String?
+    private var extendSearch = false
+    private var searchInAllCategories = true
+    private var searchItemsForGiveAway = true
+    private var searchInCategory: SportCategories?
+    private let locationManager = LocationManager.shared
+    
+    override init() {
+        super.init()
+        
+        getLocations()
+        
+    }
+    
+//    get uses location placemarks
+    func getLocations() {
+        locationManager.getCountry { (result) in
+            switch result {
+            case .success(let country):
+                self.currentCountry = country
+            case .failure(_):
+                self.currentCountry = nil
+            }
+        }
+        
+        locationManager.getAdministrativeArea { (result) in
+            switch result {
+            case .success(let adminArea):
+                self.currentAdminArea = adminArea
+            case .failure(_):
+                self.currentAdminArea = nil
+            }
+        }
+        
+        locationManager.getCity { (result) in
+            switch result {
+            case .success(let city):
+                self.currentCity = city
+            case .failure(_):
+                self.currentCity = nil
+            }
+        }
+    }
+    
+//    update search criteria if new criteria was entered
+    func updateSearchCriteria(_ extendSearch: Bool, _ searchInAllCategories: Bool, _ searchItemsForGiveAway: Bool, _ searchInCategory: SportCategories?) {
+        
+        self.extendSearch = extendSearch
+        self.searchInAllCategories = searchInAllCategories
+        self.searchItemsForGiveAway = searchItemsForGiveAway
+        self.searchInCategory = searchInCategory
+        
+    }
+    
+//    perform search
+    func performSearch(completion: @escaping(Result<DownloadedItem, SearchItemError>) -> Void) {
+        
+        
+        guard let country = currentCountry,
+            let adminArea = currentAdminArea,
+            let city = currentCity
+            else {
+                completion(.failure(.couldNotSearchInArea))
+                return
+        }
+        
+//        perform search in all categories in current city. - Default search
+        if searchInAllCategories == true && extendSearch == false && searchItemsForGiveAway == true {
+            
+            Service.shared.searchItemsForSaleInAllCategoriesInCurrentCity(country, city) { (result) in
+                switch result {
+                case .success(let item):
+                    completion(.success(item))
+                case .failure(_):
+                    completion(.failure(.failedToFetchItems))
+                }
+            }
+        }
+    }
+
+}
+
